9c8fe26110e1cc0e3086fefb0a51c5af5ea98038
ashwallsinfinity <ashley.walls@infinityworks.com>
2018-10-16 11:46:44 +0100

    test rounds json to rounds updated

--- tests/test_round_mapper.py
+++ tests/test_round_mapper.py
@@ -1,7 +1,6 @@
 import json
 import unittest
 import os.path
-from feed import app
 from feed.mappers.round_mapper import json_to_round_and_race_list
 
 test_path = os.path.abspath(os.path.dirname(__file__))
@@ -10,12 +9,18 @@ round1_path = str(test_path) + "/test_data/rounds/round1.json"
 
 class TestRoundMapper(unittest.TestCase):
 
-    def test_rounds_json_to_rounds_obj_success(self):
-        with open(round1_path) as f:
-            data = json.load(f)
-            round_id, race_list = json_to_round_and_race_list(data[0])
-            print(round_id)
+    data = {
+        "id": 1,
+        "name": "External",
+        "start_date": "Mon, 01 Oct 2018 10:00:00 GMT",
+        "end_date": "Mon, 01 Oct 2018 12:00:00 GMT",
+        "races": [1, 2, 3]
+    }
 
-        self.assertTrue(True)
+    def test_rounds_json_to_rounds_obj_success(self, data):
+        round, race_list = json_to_round_and_race_list(data)
+        round_id = round.id
+        
+        self.assertEqual(round_id, data['id'])
 
 
