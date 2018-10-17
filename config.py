bc2ca4fe5e89298d17a6ad40d7e2c2268dc9167c
James Sheard <32071575+JamesSheard@users.noreply.github.com>
2018-10-17 17:17:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ URLS = {
 }
 
 DB = {
-    'host': "localhost",
-    'user': "root",
-    'password': "psqlpass",
-    'database': "snailRacing",
+    'host': "ec2-23-21-147-71.compute-1.amazonaws.com",
+    'user': "isfktaipxvnmbp",
+    'password': "d3405d7dede20bc84142a6e336c8b476067decd768ac5ee13ccea55fa065b10c",
+    'database': "d67lulaq5muhb8",
     'port': "5432"
 }
 
