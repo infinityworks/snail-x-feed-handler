4482e9de51e66f7b71edb1ac64dae05d64a2fc33
matt-twomey-iw <matt.twomey@gmail.com>
2018-10-29 16:31:47 +0000

    Checking other branch

--- feed/mappers/racecard_mapper.py
+++ feed/mappers/racecard_mapper.py
@@ -2,5 +2,5 @@ from feed.models.racecard import Racecard
 
 
 def create_racecard_object(race_id, snail_id):
-    racecard = Racecard(race_iid, snail_id)
+    racecard = Racecard(race_id, snail_id)
     return racecard
--- feed/models/racecard.py
+++ feed/models/racecard.py
@@ -8,28 +8,15 @@ class Racecard(db.Model):
     snail_id = db.Column(db.Integer(), nullable=False)
 
 
-    def __init__(self, id, date, status, round_id):
-        self.id = id
-        self.date = date
-        self.status = status
-        self.round_id = round_id
+    def __init__(self, race_id, snail_id):
+        self.race_id = race_id
+        self.snail_id = snail_id
 
     def __repr__(self):
-        return "<Race\nid: {}\n date: {}\n status: {}\n round_id: {}>".format(self.id, self.date, self.status,
-                                                                              self.round_id)
+        return "<Racecard\nid: {}\n race_id: {}\n snail_id: {}>".format(self.id, self.race_id, self.snail_id)
 
-    def get_race(self, id):
+    def get_racecard(self, id):
         return self.query.filter_by(id=id).first()
 
-    def get_all_races(self):
-        return self.query.all()
-
-    def __repr__(self):
-        return "<Race Result\nid: {}\n position: {}\n time_to_finish: {}\n did_not_finish: {}\n race_participants_id: {}>"\
-            .format(self.id, self.position, self.time_to_finish, self.did_not_finish, self.id_race_participants)
-
-    def get_race_result(self, id):
-        return self.query.order_by(RaceResult.position).filter_by(race_participants_id=id).first()
-
-    def get_all_race_results(self):
+    def get_all_racecards(self):
         return self.query.all()
