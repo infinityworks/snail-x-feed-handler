ae17f766c89fd47d976f44f25b5d7523c3e45e02
matt-twomey-iw <matt.twomey@gmail.com>
2018-10-29 16:56:22 +0000

    changiing branched again!

--- feed/sources/racecard_source.py
+++ feed/sources/racecard_source.py
@@ -1,11 +1,13 @@
 from feed import db
-from feed.models.round import Round
+from feed.models.round import Racecard
 
 
-def save(round):
-    db.session.add(round)
+def save(racecard):
+    db.session.add(racecard)
     db.session.commit()
 
 
 def find_one_by_id(id):
-    return Round.query.get(id)
+    return Racecard.query.get(id)
+
+def find_one_by_race_id_and_snail_id(race_id, snail_id):
